57c3b7235d54564e83761432652df64a75779f34
Hélvio Filho <helviosvf@gmail.com>
2022-11-17 23:39:12 -0300

    Feat: added new route

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
+import { Register } from "../screens/Register";
 
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -12,6 +13,10 @@ export function AppRoutes() {
       <Screen
         name="home"
         component={Home}
+      />
+      <Screen
+        name="register"
+        component={Register}
       />
     </Navigator>
   );
